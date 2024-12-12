3baaf539002b80a81fd8982b9e743f618555c425
agarasia <agarasia@asu.edu>
2024-12-12 13:55:33 +0530

    Replaced the API Key to a dummy API key

--- labfetch_act1/plugins/posthog.js
+++ labfetch_act1/plugins/posthog.js
@@ -3,7 +3,7 @@ import posthog from "posthog-js";
 export default {
     install(app) {
         app.config.globalProperties.$posthog = posthog.init(
-            "phc_L6rDGwUJN5egkf3qvKPgJox9j1PwgrSWEExsi3VUDb1",
+            "phc_L6aaaaaaaaaaaaaaAAASSAFASDdakcksds",
             {
                 api_host: "https://us.i.posthog.com",
                 person_profiles: 'identified_only',
